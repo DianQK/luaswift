5c1abb0262b4b82b72dc1daede73056546186584
DianQK <dianqk@icloud.com>
2020-09-23 23:30:27 +0800

    fix: setList 中增加 bIsZero 的判断

--- Lua/vm/InstTable.swift
+++ Lua/vm/InstTable.swift
@@ -61,10 +61,12 @@ extension Instruction {
 
         _ = vm.checkStack(n: 1)
         var idx = Int64(c * LFIELDS_PER_FLUSH)
-        for j in (1...b) {
-            idx += 1
-            vm.pushValue(idx: a + j)
-            vm.setI(idx: a, i: idx) // TODO: 性能优化点？直接设置值，跳过 vm api 操作
+        if !bIsZero {
+            for j in (1...b) {
+                idx += 1
+                vm.pushValue(idx: a + j)
+                vm.setI(idx: a, i: idx) // TODO: 性能优化点？直接设置值，跳过 vm api 操作
+            }
         }
 
         if bIsZero {
