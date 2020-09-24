2360ec1e8b4af636df3c77e15ae5fd40f966f44a
DianQK <dianqk@icloud.com>
2020-09-24 19:27:57 +0800

    fix: LuaTable 改引用类似适配

--- Lua/state/APISet.swift
+++ Lua/state/APISet.swift
@@ -11,29 +11,28 @@ import Foundation
 extension LuaState {
 
     func setTable(idx: Int) {
-        var t = self.stack.get(idx: idx)
+        let t = self.stack.get(idx: idx)
         let v = self.stack.pop()
         let k = self.stack.pop()
-        self._setTable(t: &t, k: k, v: v, idx: idx)
+        self._setTable(t: t, k: k, v: v, idx: idx)
     }
 
     func setField(idx: Int, k: String) {
-        var t = self.stack.get(idx: idx)
+        let t = self.stack.get(idx: idx)
         let v = self.stack.pop()
-        self._setTable(t: &t, k: k, v: v, idx: idx)
+        self._setTable(t: t, k: k, v: v, idx: idx)
     }
 
     func setI(idx: Int, i: Int64) {
-        var t = self.stack.get(idx: idx)
+        let t = self.stack.get(idx: idx)
         let v = self.stack.pop()
-        self._setTable(t: &t, k: i, v: v, idx: idx)
+        self._setTable(t: t, k: i, v: v, idx: idx)
     }
 
     // t[k]=v
-    private func _setTable(t: inout LuaValue, k: LuaValue, v: LuaValue, idx: Int) {
+    private func _setTable(t: LuaValue, k: LuaValue, v: LuaValue, idx: Int) {
         if let tbl = t as? LuaTable {
-            tbl.put(key: k, val: v) // TODO: 产生了额外的拷贝
-            self.stack.set(idx: idx, val: tbl)
+            tbl.put(key: k, val: v)
             return
         }
         fatalError("not a table!")
